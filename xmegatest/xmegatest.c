f093b8d0ae82a61410823a22b8866c6235be0d8c
Anthony Andriano <anthonyandriano@gmail.com>
2012-07-08 15:46:54 -0600

    first commit

---
+++ xmegatest/xmegatest.c
@@ -0,0 +1,130 @@
+#include "xmegatest.h"
+
+int main(void)
+{
+	MCU_Init();
+
+	FILE USB_stdout = FDEV_SETUP_STREAM (USB_Putchar, NULL, _FDEV_SETUP_WRITE);
+	fprintf(&USB_stdout,"I'm alive...");
+
+	for(;;);
+}
+
+//Setup MCU pins and peripheral systems
+void MCU_Init( void )
+{
+	////////////////////////////////////////////////////////
+	//                  Setup GPIO Pins                   //
+	////////////////////////////////////////////////////////
+	/*    PORTA    */
+	//Set inputs and outputs
+
+	/*    PORTB    */
+	//Set inputs and outputs
+
+	/*    PORTC    */
+	//Set inputs and outputs
+	PORTC.DIRSET = MB_MOSI | MB_SCK | USB_UART_RXD;
+
+	/*    PORTD    */
+	//Set inputs and outputs
+	PORTD.DIRSET = FLASH_CS | LED0 | LED1;
+	PORTD.OUTSET = FLASH_CS | LED0 | LED1;
+
+	/*    PORTE    */
+	//Set inputs and outputs
+
+	/*    PORTR    */
+	//Set inputs and outputs
+
+	////////////////////////////////////////////////////////
+	//                 Setup Peripherals                  //
+	////////////////////////////////////////////////////////
+
+	//CLOCK SYSTEM
+	OSC.CTRL |= OSC_RC32MEN_bm; // Enable 32Mhz internal RC oscillator
+	CLK.PSCTRL = CLK_PSADIV_1_gc | CLK_PSBCDIV_1_1_gc; // Set prescalers (1,1,1)
+	while( (OSC.STATUS & OSC_RC32MRDY_bm) == 0); // Wait for the clock to be ready
+	CCP_CLK(CLK_SCLKSEL_RC32M_gc); // Enable new clock source, timing critical (4 clock cycles)
+
+	//PORTC USART
+	USARTC0.CTRLA = USART_RXCINTLVL_LO_gc; // RX interrupt is enabled and low priority
+	USARTC0.CTRLB = USART_RXEN_bm | USART_TXEN_bm; // RX and TX enabled
+	USARTC0.CTRLC = USART_CHSIZE_8BIT_gc; // 8 data bits, no parity, 1 stop bit
+	USARTC0.BAUDCTRLA = BSELH;
+	USARTC0.BAUDCTRLB = BSCALE | BSELL;
+
+	//PORTD Timer0 - Channel C & D = LED0 & 1 respectively
+	TCD0.PER = 488; //Count to 488 before overflowing and reseting the counter
+	TCD0.CTRLA = TC_CLKSEL_DIV1024_gc; // Enable timer, clk = F_CPU/1024
+	//32MHz / 1024 = 31.25KHz => 31.25KHz / 488 = 64 interrupts/sec
+	TCD0.CTRLB = TC0_CCCEN_bm | TC0_CCDEN_bm | TC0_WGMODE0_bm | TC0_WGMODE1_bm; // Enable Channel B SS PWM
+	TCD0.INTCTRLA = TC0_OVFINTLVL0_bm; // Enable Timer 0 Overflow Interrupt
+
+	//Globals
+	sine_frequency = 256;
+
+	//INTERRUPT SYSTEM	
+	PMIC.CTRL |= PMIC_LOLVLEN_bm; //Low, medium, and high interrupts enabled
+	sei(); //Enable Interrupts
+}
+
+int USB_Putchar (char c, FILE *stream)
+{
+	// Wait for the transmit buffer to be empty
+	while ( !( USARTC0.STATUS & USART_DREIF_bm) );
+ 
+	// Put the character into the transmit buffer
+	USARTC0.DATA = c; 
+
+	return 0;
+}
+
+//Data must be read to clear the interrupt flag.
+//Otherwise, the interrupt will be executed continuously.
+ISR(USARTC0_RXC_vect)
+{
+	uint8_t temp = USARTC0.DATA;
+
+	switch(temp)
+	{
+		case '0': sine_frequency = 16;
+		          break;
+
+		case '1': sine_frequency = 32;
+		          break;
+
+		case '2': sine_frequency = 64;
+				  break;
+
+		case '3': sine_frequency = 128;
+				  break;
+
+		case '4': sine_frequency = 256;
+				  break;
+
+		case '5': sine_frequency = 512;
+				  break;
+
+		case '6': sine_frequency = 1024;
+				  break;
+
+		case 'R': CCP_RST();
+				  break;
+	}
+}
+
+//TCD0 overflow interrupt. Timer frequency / Timer period = overflows per second.
+ISR(TCD0_OVF_vect)
+{
+	static uint16_t i;
+
+	//Generate a scaled sine wave value. This is crazy slow, but it works.
+	//The value will be between 0 and 255 and centered at 127.5.
+	uint16_t x = (uint16_t)(sin(2*M_PI*i/sine_frequency)*243.5+243.5);
+
+	TCD0.CCCBUF = x;
+	TCD0.CCDBUF = 488-x;
+
+	i++;
+}
---
+++ xmegatest/xmegatest.h
@@ -0,0 +1,112 @@
+#ifndef _XMEGATEST_H_
+#define _XMEGATEST_H_
+
+////////////////////////////////////////////////////////
+//                   Include Files                    //
+////////////////////////////////////////////////////////
+#include <avr/io.h>
+#include <avr/eeprom.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+#include <util/atomic.h>
+#include <stdint.h>
+#include <math.h>
+#include <stdio.h>
+
+////////////////////////////////////////////////////////
+//                  Hardware Defines                  //
+////////////////////////////////////////////////////////
+//PORTA
+#define TEST2                                PIN0_bm
+#define TEST3                                PIN1_bm
+#define TEST6                                PIN2_bm
+#define TEST5                                PIN3_bm
+#define TEST4                                PIN4_bm
+#define TEST7                                PIN5_bm
+
+//PORTB
+#define TEST12                               PIN2_bm
+
+//PORTC
+#define USB_UART_TXD                         PIN2_bm  // Input
+#define USB_UART_RXD                         PIN3_bm  // Output
+#define MB_MOSI                              PIN5_bm
+#define MB_MISO                              PIN6_bm
+#define MB_SCK                               PIN7_bm
+
+//PORTD
+#define FLASH_CS                             PIN0_bm
+#define TEST13                               PIN1_bm
+#define LED0                                 PIN2_bm
+#define LED1                                 PIN3_bm
+#define TEST11                               PIN5_bm
+#define TEST10                               PIN6_bm
+#define TEST9                                PIN7_bm
+
+//PORTE
+#define TEST8                                PIN0_bm
+
+//PORTR
+#define TEST0                                PIN0_bm
+#define TEST1                                PIN1_bm
+
+////////////////////////////////////////////////////////
+//                  Software Defines                  //
+////////////////////////////////////////////////////////
+#define BSELH                                0x2E
+#define BSELL                                0x08
+#define BSCALE                               0b10010000
+
+////////////////////////////////////////////////////////
+//                    Flash Defines                   //
+////////////////////////////////////////////////////////
+#define AT45OP_MAINMEMPAGE_READ              0xD2
+#define AT45OP_CONTARRAY_READ                0x03
+#define AT45OP_BUFFER0_READ                  0xD1
+#define AT45OP_BUFFER1_READ                  0xD3
+#define AT45OP_BUFFER0_WRITE                 0x84
+#define AT45OP_BUFFER1_WRITE                 0x87
+#define AT45OP_BUFFER0PROG_ER                0x83
+#define AT45OP_BUFFER1PROG_ER                0x86
+#define AT45OP_BUFFER0PROG_NOER              0x88
+#define AT45OP_BUFFER1PROG_NOER              0x89
+#define AT45OP_MEMPAGEPROG_BUF0              0x82
+#define AT45OP_MEMPAGEPROG_BUF1              0x85
+#define AT45OP_ERASEPAGE                     0x81
+#define AT45OP_ERASEBLOCK                    0x50
+#define AT45OP_ERASESECTOR                   0x7C
+#define AT45OP_ERASECHIP0                    0xC7
+#define AT45OP_ERASECHIP1                    0x94
+#define AT45OP_ERASECHIP2                    0x80
+#define AT45OP_ERASECHIP3                    0x9A
+#define AT45OP_MEMPAGE_TO_BUF0               0x53
+#define AT45OP_MEMPAGE_TO_BUF1               0x55
+#define AT45OP_MEMPAGE_CMP_BUF0              0x60
+#define AT45OP_MEMPAGE_CMP_BUF1              0x61
+#define AT45OP_DEEPPOWERDOWN                 0xB9
+#define AT45OP_RESUME_DEEPPWR                0xAB
+#define AT45OP_STATUSREG_READ                0xD7
+#define AT45OP_DEVICE_ID                     0x9F
+
+#define AT45_STATUSREG_RDY                   0x80
+#define AT45_STATUSREG_CMP                   0x40
+
+#define AT45_PAGE_SIZE                       528
+#define AT45_PAGE_COUNT                      4096
+#define AT45_BLOCK_COUNT                     512
+
+////////////////////////////////////////////////////////
+//                Function Prototypes                 //
+////////////////////////////////////////////////////////
+void         MCU_Init( void );
+int          USB_Putchar (char c, FILE *stream);
+void         CCP_CLK( uint8_t );
+void         CCP_RST( void );
+
+////////////////////////////////////////////////////////
+//                      Globals                       //
+////////////////////////////////////////////////////////
+volatile uint16_t sine_frequency;
+
+#endif
